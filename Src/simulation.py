9f40c5039d36b2c6c3672bf3aed9c10e02b583f8
DawitMandefro <mandefrodawit7@gmail.com>
2026-06-28 03:46:44 +0300

    Add Josephus simulation function

--- Src/simulation.py
+++ Src/simulation.py
@@ -1,1 +1,10 @@
+def josephus_simulation(n, k):
+    people = list(range(1, n + 1))
+    current_index = 0
 
+    while len(people) > 1:
+        current_index = (current_index + k - 1) % len(people)
+        eliminated = people.pop(current_index)
+        print(f"Eliminated: {eliminated}")
+
+    return people[0]
